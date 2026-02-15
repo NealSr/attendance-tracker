3af522208560f40d6126facb574fcef5ffdb2bcf
Neal Richardson Sr <Neal.Sr@outlook.com>
2026-02-15 03:40:52 -0600

    NealSr - fix vulnerabilities and dependencies

--- backend/database.js
+++ backend/database.js
@@ -1,7 +1,7 @@
 import Database from 'better-sqlite3';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const db = new Database(path.join(__dirname, 'attendance.db'));
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import db from './database.js';
 
 const app = express();
